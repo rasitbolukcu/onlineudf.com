c134421d64427235f01e416cd99c6db05b19e11a
RASIT BOLUKCU <rbolukcu@sgk.gov.tr>
2021-06-14 13:57:26 +0300

    added pdf buton

--- public/js/script.js
+++ public/js/script.js
@@ -154,7 +154,7 @@ $(document).ready(function(e){
         var worker = html2pdf();
         var element = $("<div></div>");
         $('.a4').each(function(index,item) {
-            element.append(item.clone());
+            this.clone().appendTo(element);
         });
         worker.from(element).save('dosya.pdf');
     });
